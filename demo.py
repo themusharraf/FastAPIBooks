3af0750dbd4cfd0809837ef1156df5cfa60e599a
msi <meibrohimov@gmail.com>
2024-07-04 15:11:45 +0500

    add new feature add users password hash👽

--- demo.py
+++ demo.py
@@ -8,8 +8,3 @@
 #
 # pprint(type(resp.json()) == dict)
 
-from werkzeug.security import check_password_hash, generate_password_hash
-
-s = "scrypt:32768:8:1$odrCbz48tp62xGkj$b689f5fe919ca1ac219afaa9af4681f4c6c6e7d998fb97320007d7ca060a053a04cfc81e3910f07c382fd7c2419a383ff63fb700286b3a5451a3c552dd77ab6a"
-
-print(check_password_hash(password="ags90995", pwhash=s))
